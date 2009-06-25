14c0a23b0293a7fea40c1db1f1c78a3735192bd6
NickMitin <nick.mitin@gmail.com>
2009-06-25 16:21:37 +0400

    new router + fixes

--- bmGenerator.php
+++ bmGenerator.php
@@ -8,12 +8,12 @@
     public function __construct($application, $parameters = array())
     {
       parent::__construct($application, $parameters);
-      $this->routes = $application->cacheLink->get(C_CACHE_PREFIX . 'sitePages');
-      if ($this->routes == false)
-      {
+      //$this->routes = $application->cacheLink->get(C_CACHE_PREFIX . 'sitePages');
+      //if ($this->routes == false)
+      //{
         require_once(projectRoot . '/conf/generator.conf');
-        $application->cacheLink->set(C_CACHE_PREFIX . 'sitePages', $this->routes);
-      }
+        //$application->cacheLink->set(C_CACHE_PREFIX . 'sitePages', $this->routes);
+      //}
     }
     
     public function generate($path)
@@ -26,7 +26,7 @@
         if (preg_match($route, $path, $matches))
         {
           require_once(documentRoot . $routeData['route'] . 'index.php');
-          $page = new $routeData['class']($this->application, array('userData' => $matches));
+          $page = new $routeData['class']($this->application, array('userData' => array_slice($matches, 1)));
           $result = $page->generate();
           break;
         }
