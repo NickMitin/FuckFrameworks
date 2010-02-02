41baf2f043fb69a9cff65b30f90dbc4549f84e3d
Anna <guranya@yandex.ru>
2010-02-02 07:19:43 -0700

    comments

--- bmApplicationCache.php
+++ bmApplicationCache.php
@@ -1,7 +1,15 @@
 <?php
-
+  /**
+  * Класс для работы экземпляра текущего приложения с кешем
+  */
   class bmApplicationCache extends bmCustomCache
   {
+    /**
+    * Функция выполняет sql запрос, в котором получает идентификаторы dataObjectMap и загружает объект с ними
+    * В зависимости от параметра $load, функция или возвращает идентификатор объекта или загруженный объект со всеми полями
+    * @param bmApplication $application экземпляр текущего приложения
+    * @param boolean $load флаг, указывающий необходимо ли загружать полученные объекты. По умолчанию true
+    */
     public function getDataObjectMaps(bmApplication $application, $load = true)
     {
       $cacheKey = 'dataObjectMaps';
