82011499383350a685f762577dc599c4ec374df0
vir-mir <virmir49@gmail.com>
2014-08-08 13:11:11 -0400

    image: flash session

---
+++ bmFlashSession.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: vir-mir
+ * Date: 08.08.14
+ * Time: 15:31
+ */
+
+trait bmFlashSession
+{
+
+	public function setFlash($name, $value)
+	{
+		$this->application->cgi->addCookie($name, serialize($value), true, '/', '', time() + 10);
+	}
+
+	public function getFlash($name)
+	{
+		$result = $this->application->cgi->getGPC($name, null);
+		$result = $result ? unserialize($result) : null;
+		$this->application->cgi->deleteCookie($name);
+		return $result;
+	}
+
+} 
