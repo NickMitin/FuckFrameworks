d1a5566225559e09d0a3cf22462e63deea6a3210
Mikhail Lisnyak <xpundel@gmail.com>
2015-05-15 17:21:16 +0300

    add mbucfirst

--- bmTools.php
+++ bmTools.php
@@ -180,4 +180,12 @@ class bmTools
 
 		return $out;
 	}
+
+	static function mbUcfirst($string, $encoding = 'utf8')
+	{
+		$strlen = mb_strlen($string, $encoding);
+		$firstChar = mb_substr($string, 0, 1, $encoding);
+		$then = mb_substr($string, 1, $strlen - 1, $encoding);
+		return mb_strtoupper($firstChar, $encoding) . $then;
+	}
 }
