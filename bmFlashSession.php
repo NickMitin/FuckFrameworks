0b1781259b398102d2679e5209eda4e125971022
Mikhail Lisnyak <xpundel@gmail.com>
2015-05-18 19:18:11 +0300

    add flash cookie prefix

--- bmFlashSession.php
+++ bmFlashSession.php
@@ -1,24 +1,25 @@
 <?php
+
 /**
  * Created by PhpStorm.
  * User: vir-mir
  * Date: 08.08.14
  * Time: 15:31
  */
-
 trait bmFlashSession
 {
 
 	public function setFlash($name, $value)
 	{
-		$this->application->cgi->addCookie($name, serialize($value), true, '/', '', time() + 10);
+		$this->application->cgi->addCookie('flash_session_' . $name, serialize($value), true, '/', '', time() + 10);
 	}
 
 	public function getFlash($name)
 	{
-		$result = $this->application->cgi->getGPC($name, null);
+		$result = $this->application->cgi->getGPC('flash_session_' . $name, null);
 		$result = $result ? unserialize($result) : null;
-		$this->application->cgi->deleteCookie($name);
+		$this->application->cgi->deleteCookie('flash_session_' . $name);
+
 		return $result;
 	}
 
